47f45559c4acf923b993955d6188ec83f077b334
Russ Perry <rperry1886@gmail.com>
2020-01-01 10:27:20 -0500

    Add HTML Preview Functionality
    When you generate the pie chart, the HTML preview box will give you the html code needed for the key to copy into your own projects

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ const piece3Percent = document.getElementById("item3-percent");
 const piece4Percent = document.getElementById("item4-percent");
 const piece5Percent = document.getElementById("item5-percent");
 
+// Pie Chart
+const pieChart = document.getElementById("pie-preview");
+
+// HTML and CSS Previews
+const htmlPreview = document.getElementById("html-preview");
+const cssPreview = document.getElementById("css-preview");
+
 function generate() {
+  updateKey();
+  updatePercent();
+  updateHTML();
+}
+
+function updateKey() {
   piece1Key.innerHTML = `<span class="dot dot1"></span>${piece1Name.value}`;
   piece2Key.innerHTML = `<span class="dot dot2"></span>${piece2Name.value}`;
   piece3Key.innerHTML = `<span class="dot dot3"></span>${piece3Name.value}`;
@@ -27,39 +40,37 @@ function generate() {
   piece5Key.innerHTML = `<span class="dot dot5"></span>${piece5Name.value}`;
 }
 
-// Change the text in the key when you type in the boxes.
-// piece1Name.onkeyup = () => {
-//   if (piece1Name.value === "") {
-//     piece1Key.innerHTML = `<span class="dot dot1"></span>Item 1`;
-//   } else {
-//     piece1Key.innerHTML = `<span class="dot dot1"></span>${piece1Name.value}`;
-//   }
-// };
-// piece2Name.onkeyup = () => {
-//   if (piece2Name.value === "") {
-//     piece2Key.innerHTML = `<span class="dot dot2"></span>Item 2`;
-//   } else {
-//     piece2Key.innerHTML = `<span class="dot dot2"></span>${piece2Name.value}`;
-//   }
-// };
-// piece3Name.onkeyup = () => {
-//   if (piece3Name.value === "") {
-//     piece3Key.innerHTML = `<span class="dot dot3"></span>Item 3`;
-//   } else {
-//     piece3Key.innerHTML = `<span class="dot dot3"></span>${piece3Name.value}`;
-//   }
-// };
-// piece4Name.onkeyup = () => {
-//   if (piece4Name.value === "") {
-//     piece4Key.innerHTML = `<span class="dot dot4"></span>Item 4`;
-//   } else {
-//     piece4Key.innerHTML = `<span class="dot dot4"></span>${piece4Name.value}`;
-//   }
-// };
-// piece5Name.onkeyup = () => {
-//   if (piece5Name.value === "") {
-//     piece5Key.innerHTML = `<span class="dot dot5"></span>Item 5`;
-//   } else {
-//     piece5Key.innerHTML = `<span class="dot dot5"></span>${piece5Name.value}`;
-//   }
-// };
+function updatePercent() {
+  let percent1 = parseInt(piece1Percent.value);
+  let percent2 = parseInt(piece2Percent.value) + percent1;
+  let percent3 = parseInt(piece3Percent.value) + percent2;
+  let percent4 = parseInt(piece4Percent.value) + percent3;
+  let percent5 = parseInt(piece5Percent.value) + percent4;
+  console.log(percent1, percent2, percent3, percent4, percent5);
+  pieChart.style.background = `conic-gradient(
+    red 0% ${percent1}%,
+    green ${percent1}% ${percent2}%,
+    blue ${percent2}% ${percent3}%,
+    yellow ${percent3}% ${percent4}%,
+    purple ${percent4}% ${percent5}%
+  )`;
+}
+
+function updateHTML() {
+  htmlPreview.innerHTML = `
+  <xmp>
+    <div class="pie-chart">
+    <div class="key">
+      <p>Key</p>
+      <ul>
+        <li id="item1-key"><span class="dot dot1"></span>${piece1Name.value}</li>
+        <li id="item2-key"><span class="dot dot2"></span>${piece2Name.value}</li>
+        <li id="item3-key"><span class="dot dot3"></span>${piece3Name.value}</li>
+        <li id="item4-key"><span class="dot dot4"></span>${piece4Name.value}</li>
+        <li id="item5-key"><span class="dot dot5"></span>${piece5Name.value}</li>
+      </ul>
+    </div>
+    <div id="pie-preview"></div>
+  </xmp>
+  `;
+}
