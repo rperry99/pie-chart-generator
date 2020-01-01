ffcd859d3351457ec3da88a6cc2006605774e5af
Russ Perry <rperry1886@gmail.com>
2019-12-31 21:11:56 -0500

    Add Generate button, refactor js
    Change the process so now the user presses a generate button once the form is filled out. This got rid of the repetitiveness of a lot of the js

--- index.js
+++ index.js
@@ -19,39 +19,47 @@ const piece3Percent = document.getElementById("item3-percent");
 const piece4Percent = document.getElementById("item4-percent");
 const piece5Percent = document.getElementById("item5-percent");
 
+function generate() {
+  piece1Key.innerHTML = `<span class="dot dot1"></span>${piece1Name.value}`;
+  piece2Key.innerHTML = `<span class="dot dot2"></span>${piece2Name.value}`;
+  piece3Key.innerHTML = `<span class="dot dot3"></span>${piece3Name.value}`;
+  piece4Key.innerHTML = `<span class="dot dot4"></span>${piece4Name.value}`;
+  piece5Key.innerHTML = `<span class="dot dot5"></span>${piece5Name.value}`;
+}
+
 // Change the text in the key when you type in the boxes.
-piece1Name.onkeyup = () => {
-  if (piece1Name.value === "") {
-    piece1Key.innerHTML = `<span class="dot dot1"></span>Item 1`;
-  } else {
-    piece1Key.innerHTML = `<span class="dot dot1"></span>${piece1Name.value}`;
-  }
-};
-piece2Name.onkeyup = () => {
-  if (piece2Name.value === "") {
-    piece2Key.innerHTML = `<span class="dot dot2"></span>Item 2`;
-  } else {
-    piece2Key.innerHTML = `<span class="dot dot2"></span>${piece2Name.value}`;
-  }
-};
-piece3Name.onkeyup = () => {
-  if (piece3Name.value === "") {
-    piece3Key.innerHTML = `<span class="dot dot3"></span>Item 3`;
-  } else {
-    piece3Key.innerHTML = `<span class="dot dot3"></span>${piece3Name.value}`;
-  }
-};
-piece4Name.onkeyup = () => {
-  if (piece4Name.value === "") {
-    piece4Key.innerHTML = `<span class="dot dot4"></span>Item 4`;
-  } else {
-    piece4Key.innerHTML = `<span class="dot dot4"></span>${piece4Name.value}`;
-  }
-};
-piece5Name.onkeyup = () => {
-  if (piece5Name.value === "") {
-    piece5Key.innerHTML = `<span class="dot dot5"></span>Item 5`;
-  } else {
-    piece5Key.innerHTML = `<span class="dot dot5"></span>${piece5Name.value}`;
-  }
-};
+// piece1Name.onkeyup = () => {
+//   if (piece1Name.value === "") {
+//     piece1Key.innerHTML = `<span class="dot dot1"></span>Item 1`;
+//   } else {
+//     piece1Key.innerHTML = `<span class="dot dot1"></span>${piece1Name.value}`;
+//   }
+// };
+// piece2Name.onkeyup = () => {
+//   if (piece2Name.value === "") {
+//     piece2Key.innerHTML = `<span class="dot dot2"></span>Item 2`;
+//   } else {
+//     piece2Key.innerHTML = `<span class="dot dot2"></span>${piece2Name.value}`;
+//   }
+// };
+// piece3Name.onkeyup = () => {
+//   if (piece3Name.value === "") {
+//     piece3Key.innerHTML = `<span class="dot dot3"></span>Item 3`;
+//   } else {
+//     piece3Key.innerHTML = `<span class="dot dot3"></span>${piece3Name.value}`;
+//   }
+// };
+// piece4Name.onkeyup = () => {
+//   if (piece4Name.value === "") {
+//     piece4Key.innerHTML = `<span class="dot dot4"></span>Item 4`;
+//   } else {
+//     piece4Key.innerHTML = `<span class="dot dot4"></span>${piece4Name.value}`;
+//   }
+// };
+// piece5Name.onkeyup = () => {
+//   if (piece5Name.value === "") {
+//     piece5Key.innerHTML = `<span class="dot dot5"></span>Item 5`;
+//   } else {
+//     piece5Key.innerHTML = `<span class="dot dot5"></span>${piece5Name.value}`;
+//   }
+// };
